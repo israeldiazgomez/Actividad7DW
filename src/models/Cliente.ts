1613d8736d372a4968b47fb5a3d57d2697b293fe
YilberthAndres <yilberthgalarza444@gmail.com>
2021-10-13 16:01:13 -0500

    yilberth

--- src/models/Cliente.ts
+++ src/models/Cliente.ts
@@ -15,3 +15,30 @@ export interface ClienteI {
     correo: string;
     clave: string;
 }
+
+Cliente.init (
+    {
+        nombre: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        apellido: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        correo: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        clave: {
+            type: DataTypes.STRING,
+            allowNull: false
+        }
+    },
+    {
+        tableName: "clientes",
+        sequelize: database,
+        timestamps: true
+
+    }
+)
