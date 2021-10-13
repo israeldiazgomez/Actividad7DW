e39a00d0df64896a48f2e76d6f4ea9faef778d8d
israel25D <ijesusdiaz@uniguajira.edu.co>
2021-10-13 12:20:23 -0500

    israel

---
+++ src/database/db.ts
@@ -0,0 +1,25 @@
+const Sequelize = require('sequelize');
+
+
+const BD_NAME = 'db_ositofeliz';
+
+const BD_USER = 'grupo4';
+const BD_PASS = 'Grupo4.123';
+
+export const database = new Sequelize(
+
+    BD_NAME,
+    BD_USER,
+    BD_PASS,
+
+    {
+        host:'localhost',
+        dialect: 'mysql',
+        port: 3306
+    }
+);
+
+database.sync( {force: true})
+    .then(function(){
+        console.log('Creda correctamente')
+    })
