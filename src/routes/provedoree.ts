937fb8853581fee24b52c52933b07ce2900e819b
leinys <lepagifo@hotmail.com>
2021-10-15 13:42:00 -0500

    leinys

--- src/routes/provedoree.ts
+++ src/routes/provedoree.ts
@@ -5,11 +5,11 @@ import { ProveedoreController } from "../controllers/proveedore.controller";
 export class ProveedoreRoutes{
     public proveedoreController: ProveedoreController = new ProveedoreController();
     public routes(app: Application): void{
-        // app.route('/proveedore').get(this.proveedoreController.index);
-        app.route('/proveedore').get(this.proveedoreController.getProveedores);
-
-        //crear usuario 
+        
+        app.route('/proveedore').get(this.proveedoreController.getProveedores); 
         app.route('/crearproveedor').post(this.proveedoreController.createProveedores);
+        app.route('/borrarproveedor').delete(this.proveedoreController.borrarProveedor);
+        
     }
 
 } 
