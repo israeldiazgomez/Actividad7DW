b7a42d8696e798e7dac0c8ce3fd8fde3df2cad8d
Marlon-Alarcon <mralarcon@uniguajira.edu.co>
2021-10-17 15:57:01 -0500

    marlon

--- src/controllers/compra.controller.ts
+++ src/controllers/compra.controller.ts
@@ -2,9 +2,17 @@ import { Request, Response } from "express";
 import { Compra } from '../models/Compra'
 
 export class CompraController {
-    public index(req: Request, res: Response){
-        Compra.findAll({})
-            .then((compras: Array<Compra>) => res.json(compras))
-            .catch((err: Error) => res.status(500).json(err));
+    
+    public async getCompras(req: Request, res: Response){
+
+        try {
+            const compras = await Compra.findAll()
+            if(!compras){
+                res.status(400).json({ msg: 'no conexion'})
+            }
+            return res.status(200).json({compras})
+        } catch (error) {
+            res.status(500).json({ msg: 'Error interno'})
+        }
     }
 }
